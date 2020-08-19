935c2b9b34a0b020bdcd073d4770e9e296f5c7c7
Nick Sherman <ogleffeckt2@yahoo.com>
2020-08-19 19:32:39 -0400

    wired up controller & get decorator around login route

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,8 +1,10 @@
 import { Router, Request, Response, NextFunction } from 'express';
+import { get } from './decorators/routes';
+import { controller } from './decorators/controller';
 
-@controller('/')
+@controller('/auth')
 class LoginController {
-  @get('/login')
+	@get('/login')
 	getLogin(req: Request, res: Response): void {
 		res.send(`
     <form method="POST">
