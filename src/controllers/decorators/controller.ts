4a188a02bbd97b4f79e11fae43e7cf9a3b32e8dd
Nick Sherman <ogleffeckt2@yahoo.com>
2020-08-28 16:51:04 -0400

    wired up automated validation & tested it

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -6,8 +6,20 @@ import { Request, Response, RequestHandler, NextFunction } from 'express';
 
 function bodyValidators(keys: string): RequestHandler {
 	return function(req: Request, res: Response, next: NextFunction) {
-		
-	}
+		if (!req.body) {
+			res.status(422).send('Invalid request');
+			return;
+		}
+
+		for (let key of keys) {
+			if (!req.body[key]) {
+				res.status(422).send(`Missing property ${key}`);
+				return;
+			}
+		}
+
+		next();
+	};
 }
 
 export function controller(routePrefix: string) {
@@ -19,9 +31,12 @@ export function controller(routePrefix: string) {
 			const path = Reflect.getMetadata(MetadataKeys.path, target.prototype, key);
 			const method: Methods = Reflect.getMetadata(MetadataKeys.method, target.prototype, key);
 			const middlewares = Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) || [];
+			const requiredBodyProps = Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) || [];
+
+			const validator = bodyValidators(requiredBodyProps);
 
 			if (path) {
-				router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
+				router[method](`${routePrefix}${path}`, ...middlewares, validator, routeHandler);
 			}
 		}
 	};
