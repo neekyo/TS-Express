fbea3f75ace8b90526d32c909a4b725dd8a20fb1
Nick Sherman <ogleffeckt2@yahoo.com>
2020-08-31 16:58:47 -0400

    created a RootController to handle route re-factors

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -16,29 +16,6 @@ function requireAuth(req: Request, res: Response, next: NextFunction): void {
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-	if (req.session && req.session.loggedIn) {
-		res.send(`
-			<div>
-				<div>You are logged in</div>
-				<a href="/logout">Logout</a>
-			</div>
-		`);
-	} else {
-		res.send(`
-			<div>
-				<div>You are not logged in</div>
-				<a href="/login">Login</a>
-			</div>
-		`);
-	}
-});
-
-router.get('/logout', (req: Request, res: Response) => {
-	req.session = null;
-	res.redirect('/');
-});
-
 router.get('/protected', requireAuth, (req: Request, res: Response) => {
 	res.send('You are logged in & protected');
 });
